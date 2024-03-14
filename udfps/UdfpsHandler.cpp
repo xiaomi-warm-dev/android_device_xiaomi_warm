d067f51f6176ab9a839f9c21e86c8f034f91c337
zarakinyu <zarakinyu67@gmail.com>
2024-03-14 13:50:54 +0100

    sm4635-common: udfps: Add comments about setFingerDown usage
    Change-Id: Ifb0394a887cdabdad20208b5deb2386e7cc7f55a

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -50,17 +50,20 @@ class XiaomiSM8650UdfpsHandler : public UdfpsHandler {
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
         LOG(INFO) << __func__;
+        // Ensure touchscreen is aware of the press state, ideally this is not needed
         setFingerDown(true);
     }
 
     void onFingerUp() {
         LOG(INFO) << __func__;
+        // Ensure touchscreen is aware of the press state, ideally this is not needed
         setFingerDown(false);
     }
 
     void onAcquired(int32_t result, int32_t vendorCode) {
         LOG(INFO) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
         if (result != FINGERPRINT_ACQUIRED_VENDOR) {
+            // Set finger as up to disable HBM already, even if the finger is still pressed
             setFingerDown(false);
             if (result == FINGERPRINT_ACQUIRED_GOOD) setFodStatus(FOD_STATUS_OFF);
         } else if (vendorCode == 21 || vendorCode == 23) {

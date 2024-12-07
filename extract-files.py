0d7230246d9efc46ef722bd9d6ba2f8999585ae5
zarakinyu <zarakinyu67@gmail.com>
2024-12-07 15:59:17 -0600

    sm8550-common: Bump libwfdservice to android.media.audio.common.types-V4
    Change-Id: I81060d8c73430a9598c676e18dc04e31a2f31c0c

--- extract-files.py
+++ extract-files.py
@@ -97,7 +97,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libinput_shim.so')
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libqtikeymint.so'): blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()

a067d10122c23f66b4126d8f54a5f50fa37a0ecb
zarakinyu <zarakinyu67@gmail.com>
2025-03-21 11:18:16 +0800

    sm4635-common: Address seccomp failures for qsap_location
    Change-Id: Ia6da59107f1c81fc9af55bbf73d2252016fd17df

--- extract-files.py
+++ extract-files.py
@@ -126,6 +126,9 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/etc/media_codecs_pineapple.xml': blob_fixup()
         .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
+    'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
+        .add_line_if_missing('sched_get_priority_min: 1')
+        .add_line_if_missing('sched_get_priority_max: 1'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()

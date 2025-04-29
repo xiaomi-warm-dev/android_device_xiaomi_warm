082eac402c72c51363050431745fcee3b90e9d44
zarakinyu <zarakinyu67@gmail.com>
2025-04-29 23:37:50 +0100

    sm4635-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,9 +163,6 @@ TARGET_KERNEL_EXT_MODULES := \
  	qcom/opensource/mm-sys-kernel/ubwcp \
  	nxp/opensource/driver
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/mmi_charging_enable
-
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := pineapple

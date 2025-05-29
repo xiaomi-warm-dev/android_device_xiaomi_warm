7fb2522c970db1426962a104dfb78a5b41690a46
zarakinyu <zarakinyu67@gmail.com>
2025-05-29 14:13:50 +0700

    sm4635-common: Drop old XiaomiParts
    Signed-off-by: vherawidatama <vherawidatama@gmail.com>

--- common.mk
+++ common.mk
@@ -257,10 +257,6 @@ PRODUCT_PACKAGES += \
     WifiResTarget \
     WifiResTarget_spf
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true

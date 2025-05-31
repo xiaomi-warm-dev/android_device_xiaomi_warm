143776f19ecb372ac631568f9eb3cb9c50ed14b7
zarakinyu <zarakinyu67@gmail.com>
2025-05-31 17:34:21 +0530

    Revert "sm4635-common: parts: Introduce RefreshRate QS tile"
    This reverts commit 54c084a45bb9c299ebe8a0f645aa0b7bad0ec00c.

--- parts/src/org/lineageos/settings/TileHandlerActivity.java
+++ parts/src/org/lineageos/settings/TileHandlerActivity.java
@@ -33,8 +33,6 @@ import org.lineageos.settings.autohbm.AutoHbmTileService;
 import org.lineageos.settings.autohbm.HbmTileService;
 import org.lineageos.settings.thermal.ThermalSettingsActivity;
 import org.lineageos.settings.thermal.ThermalTileService;
-import org.lineageos.settings.refreshrate.RefreshActivity;
-import org.lineageos.settings.refreshrate.RefreshTileService;
 import org.lineageos.settings.gamebar.GameBarSettingsActivity;
 import org.lineageos.settings.gamebar.GameBarTileService;
 import org.lineageos.settings.turbocharging.TurboChargingActivity;
@@ -50,7 +48,6 @@ public final class TileHandlerActivity extends Activity {
         TILE_ACTIVITY_MAP.put(AutoHbmTileService.class.getName(), AutoHbmActivity.class);
         TILE_ACTIVITY_MAP.put(HbmTileService.class.getName(), AutoHbmActivity.class);
         TILE_ACTIVITY_MAP.put(ThermalTileService.class.getName(), ThermalSettingsActivity.class);
-        TILE_ACTIVITY_MAP.put(RefreshTileService.class.getName(), RefreshActivity.class);
         TILE_ACTIVITY_MAP.put(GameBarTileService.class.getName(), GameBarSettingsActivity.class);
         TILE_ACTIVITY_MAP.put(TurboChargingTile.class.getName(), TurboChargingActivity.class);
     }

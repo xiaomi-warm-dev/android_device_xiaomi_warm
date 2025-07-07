3c969b81b2854e9cf1691e1aad2f60d686db186a
zarakinyu <zarakinyu67@gmail.com>
2025-07-07 08:59:01 +0530

    sm4635-common: Update from OOS 11.C.79
    Change-Id: I9aaf4842699be8f8a7a138880eed9172191888cc
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -94,7 +94,7 @@ blob_fixups: blob_fixups_user_type = {
         .remove_needed('android.hardware.graphics.common-V4-ndk.so'),
     ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libSuperRaw.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libyuv2.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    ('odm/lib64/libEIS.so', 'odm/lib64/libHIS.so', 'odm/lib64/libOPAlgoCamFaceBeautyCap.so', 'odm/lib64/libOGLManager.so'): blob_fixup()
+    ('odm/lib64/libEIS.so', 'odm/lib64/libEISLive.so', 'odm/lib64/libHIS.so', 'odm/lib64/libOPAlgoCamFaceBeautyCap.so', 'odm/lib64/libOGLManager.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
         .clear_symbol_version('AHardwareBuffer_lock')

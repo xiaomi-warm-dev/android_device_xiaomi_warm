0d219d707da13daf916d7630d7ebc3c8efb283d2
zarakinyu <zarakinyu67@gmail.com>
2025-07-30 16:19:00 +0000

    sm4635-common: Import kernel post-boot scripts for cliffs
    Removed OPlus changes
    
    Change-Id: I2eacb324f90990b78f4482fba03779271ab6ab23

--- common.mk
+++ common.mk
@@ -201,6 +201,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot-cliffs_default_3_4_1.sh \
     init.kernel.post_boot-pineapple.sh \
     init.kernel.post_boot-pineapple_default_2_3_2_1.sh \
     init.oplus.rc \

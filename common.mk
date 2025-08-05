454aeee92d7cf4e696d402bdca5bd3aad2c818d1
zarakinyu <zarakinyu67@gmail.com>
2025-08-05 12:57:12 +0800

    sm4635-common: Support IFAA fingerprint payment
    Change-Id: I3961dc0993e995b4f77b55aace8ca8625bd3f2c7

--- common.mk
+++ common.mk
@@ -152,6 +152,9 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
+PRODUCT_PACKAGES += \
+    IFAAService
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 

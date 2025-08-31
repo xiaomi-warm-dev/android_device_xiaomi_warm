b062e916547f8a43cbb20bc961de49630e9e2b26
zarakinyu <zarakinyu67@gmail.com>
2025-08-31 22:42:47 +0200

    sm4635-common: Set required deps for QTI adsprpc and composer service
    Change-Id: I80cabd805d4b4d182eb42bcb2bd6a79c3599523f

--- common.mk
+++ common.mk
@@ -57,7 +57,6 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener \
     sound_trigger.primary.pineapple \
-    vendor.qti.audio-adsprpc-service.rc \
     vendor.qti.hardware.AGMIPC@1.0-impl
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8650/audio/primary-hal
@@ -125,8 +124,6 @@ PRODUCT_PACKAGES += \
     libgpu_tonemapper \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
-    vendor.qti.hardware.display.composer-service.rc \
-    vendor.qti.hardware.display.composer-service.xml \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor \
     vendor.qti.hardware.display.config-V2-ndk.vendor \
     vendor.qti.hardware.display.demura-service \

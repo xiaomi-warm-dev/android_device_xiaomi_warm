5dd99566f9d92493bfde2c15d7261c0d38d601f7
zarakinyu <zarakinyu67@gmail.com>
2025-10-07 21:32:02 +0530

    sm4635-common: Migrate QTI power HAL to soong_config_set
    Change-Id: Ib80c15ee523130011147d378f665592dae501ec2

--- common.mk
+++ common.mk
@@ -325,6 +325,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/pineapple/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+$(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
+
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor # Needed by CNE app

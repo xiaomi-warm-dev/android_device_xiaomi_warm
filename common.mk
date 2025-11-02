4201c7439dea5fd1e998ceb9b58a42f735c969f1
zarakinyu <zarakinyu67@gmail.com>
2025-11-02 15:59:54 +0000

    sm4635-common: Move manifests to vintf directory
    Change-Id: I2a82e3ae75a1293318efeea0519cfbe64eb4b364

--- common.mk
+++ common.mk
@@ -485,19 +485,19 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE += device/oneplus/sm8650-common/framework_manifest.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += device/oneplus/sm8650-common/vintf/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa.xml \
     $(AUDIO_HAL_DIR)/configs/common/manifest_non_qmaa_extn.xml \
-    $(LOCAL_PATH)/manifest_pineapple.xml
+    $(LOCAL_PATH)/vintf/manifest_pineapple.xml
 
 ifneq ($(TARGET_IS_TABLET),true)
 DEVICE_MANIFEST_FILE += \
-    $(LOCAL_PATH)/network_manifest.xml
+    $(LOCAL_PATH)/vintf/network_manifest.xml
 
 ODM_MANIFEST_FILES := \
-    $(LOCAL_PATH)/network_manifest_odm.xml
+    $(LOCAL_PATH)/vintf/network_manifest_odm.xml
 endif
 
 # WiFi
